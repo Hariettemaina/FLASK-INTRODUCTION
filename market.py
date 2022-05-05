b282169f513ce8a003fdd4bd919f450b5657f88e
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-05-05 23:45:56 +0300

    navbar and style page to black

--- market.py
+++ market.py
@@ -3,9 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home ')
 def home_page():
     return render_template('home.html')
 
-@app.route('/about/<username>')
-def about_page(username):
-    return f'<h2>This is the about page of the {username}</h2>'
